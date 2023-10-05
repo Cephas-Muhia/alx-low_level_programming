18f1e9b46267d0c44f0a8d7f8b30c829b9b20571
Cephas-Muhia <cephaschronixx@gmail.com>
2023-10-05 09:39:32 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-  * main- this is a main funtion
-  *
-  * Return Always 0
-  */
-int main (void)
+ * main -This is the  main function
+ *
+ *Return: always returns 0 when succesifull
+ *
+ */
+int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle);
+printf("with proper grammar, but the outcome is a piece of art,"\n);
 return (0);
 }

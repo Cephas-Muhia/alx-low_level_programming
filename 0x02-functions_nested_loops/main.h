9f6176cba562cd33145b43efb719bd2b6b99a5c8
Cephas-Muhia <cephaschronixx@gmail.com>
2023-10-12 08:04:44 -0700

    printing last digit.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <ctype.h>
+
+int _putchar(char c);
+int print_last_digit(int n);
 int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
 void print_alphabet_x10(void);
 void print_alphabet(void);
-int _putchar(char c);
 #endif

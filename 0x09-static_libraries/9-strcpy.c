652c16f83b908a263ed783bce97d31c7edab14d9
Cephas- Muhia <cephaschronixx@gmal.com>
2023-10-28 23:01:10 -0700

    libmy.a task

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strcpy - Copies a string pointed to by src to a buffer pointed to by dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: A pointer to the destination buffer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *original_dest = dest; /* Store the original dest address */
+
+	if (dest == NULL || src == NULL)
+	{
+		return (NULL); /* Handle null pointers */
+	}
+
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0'; /* Ensure null termination */
+	return (original_dest);
+}
+

67f52d68ef5de655739bd7835bb3b00a6f5f026c
Cephas- Muhia <cephaschronixx@gmal.com>
2023-10-28 22:36:12 -0700

    copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,7 +8,7 @@
  *
  * Return: A pointer to the destination buffer.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	char *original_dest = dest; /* Store the original dest address */
 

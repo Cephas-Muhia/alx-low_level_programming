9282b53726847fe75e5c8afe35eb07afc4f1a67d
Cephas- Muhia <cephaschronixx@gmal.com>
2023-11-21 22:42:48 -0800

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,15 +12,13 @@ size_t print_listint(const listint_t *h)
 	size_t n_nodes = 0;
 
 	if (!h)
-		return (0);
-
+	return (0);
 	while (h)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		n_nodes++;
+	printf("%d\n", h->n);
+	h = h->next;
+	n_nodes++;
 	}
-	return (n_nodes);
+return (n_nodes);
 }
-
 

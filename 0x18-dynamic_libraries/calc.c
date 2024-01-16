7683a55db404c80c65aa5f73df7c823f2d9bdf5b
Cephas-Muhia <cephaschronixx@gmail.com>
2024-01-16 09:42:38 -0800

    Using shared library in python that contains c function

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ *
+ * mod - modulas of two integers
+ * @a: first number
+ * @b: second number
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return(a % b);
+}
+/**
+ *
+ * add - addition of two integers
+ * @a: first number
+ * @b: second number
+ * Return: final answer
+ */
+int add(int a, int b)
+{
+        return(a + b);
+}
+/**
+ *
+ * sub - substraction of two integers
+ * @a: first number
+ * @b: second number
+ * Return: final answer
+ */
+int sub(int a, int b)
+{
+        return(a - b);
+}
+/**
+ *
+ * mul - multiplacation of two integers
+ * @a: first number
+ * @b: second number
+ * Return: final answer
+ */
+int mul(int a, int b)
+{
+        return(a * b);
+}
+/**
+ *
+ * div - division of two integers
+ * @a: first number
+ * @b: second number
+ * Return: final answer
+ */
+int div(int a, int b)
+{
+        return(a / b);
+}
